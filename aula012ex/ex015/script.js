030d10ebabd93ff2905d5528caa47d860deee84a
Vagner Santana <141592406+vagnerns@users.noreply.github.com>
2023-12-27 16:00:21 -0300

    concluído

--- aula012ex/ex015/script.js
+++ aula012ex/ex015/script.js
@@ -1,1 +1,49 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('itxtano')
+    var res = window.document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERROR] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = window.document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = window.document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/crianca-h.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/jovem-h.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto-h.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'imagens/crianca-m.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/jovem-m.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/adulto-m.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Identificamos ${gênero} com idade ${idade} anos.`
+        res.appendChild(img)
+    }
+}
