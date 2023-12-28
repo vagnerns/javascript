de1265350eaca6e3a462ae743bb433bea47306d4
Vagner Santana <141592406+vagnerns@users.noreply.github.com>
2023-12-28 14:30:06 -0300

    aula014

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,3 +1,18 @@
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 10)
+
+
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+}
+*/
+
 /*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
