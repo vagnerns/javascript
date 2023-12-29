611190f23c9547342b0f6ef3633e2911cb76efdf
Vagner Santana <141592406+vagnerns@users.noreply.github.com>
2023-12-29 14:52:36 -0300

    Update script.js

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -2,8 +2,27 @@ function contar() {
     let ini = window.document.getElementById('itxti')
     let fim = window.document.getElementById('itxtf')
     let passo = window.document.getElementById('itxtp')
+    let res = window.document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERROR], Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (1 < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        } 
+        res.innerHTML += `\u{1f3c1}`   
+        
     }
 }
